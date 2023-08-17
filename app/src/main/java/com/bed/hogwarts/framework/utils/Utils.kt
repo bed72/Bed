93bede76ab8b099469e93a5ff1f2a40fb748cc09
bed72 <gjramos1000@gmail.com>
2023-08-16 21:57:49 -0300

    :tada: Brabo d+

--- app/src/main/java/com/bed/hogwarts/framework/utils/Utils.kt
+++
@@ -1,9 +0,0 @@
-package com.bed.hogwarts.framework.utils
-
-import kotlinx.datetime.Clock
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
-
-object Utils {
-    val dateNow get() = Clock.System.now().toLocalDateTime(TimeZone.UTC).date
-}
